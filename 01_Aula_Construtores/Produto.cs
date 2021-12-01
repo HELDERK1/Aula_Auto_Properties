5390707306b47cc6d58c46541ef1b44264ec0593
Helderk1 <helderk2011@hotmail.com>
2021-12-01 20:26:20 -0300

    first commit

---
+++ 01_Aula_Construtores/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_Aula_Construtores
+{
+    class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
